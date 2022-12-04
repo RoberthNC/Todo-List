e74a13cbb3fa755e1a554c9b5ad2f42f5d37eb0d
Roberto Núñez <robertonc1599@gmail.com>
2022-12-04 14:17:55 -0500

    Agregando el color

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -9,31 +9,31 @@ const preguntas = [
         choices:[
             {
                 value:'1',
-                name:'1. Crear una tarea'
+                name:`${"1.".blue} Crear una tarea`
             },
             {
                 value:'2',
-                name:'2. Listar tareas'
+                name:`${"2.".blue} Listar tareas`
             },
             {
                 value:'3',
-                name:'3. Listar tareas completadas'
+                name:`${"3.".blue} Listar tareas completadas`
             },
             {
                 value:'4',
-                name:'4. Listar tareas pendientes'
+                name:`${"4.".blue} Listar tareas pendientes`
             },
             {
                 value:'5',
-                name:'5. Completar tarea(s)'
+                name:`${"5.".blue} Completar tarea(s)`
             },
             {
                 value:'6',
-                name:'6. Borrar tarea'
+                name:`${"6.".blue} Borrar tarea`
             },
             {
                 value:'0',
-                name:'0. Salir'
+                name:`${"0.".blue} Salir`
             }
         ]
     }
